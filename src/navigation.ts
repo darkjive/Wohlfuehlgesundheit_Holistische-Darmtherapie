248fceb4c59081da0b52029a55c6a6cf8b274b0a
Al Ain <cosmwave@gmail.com>
2025-09-05 10:20:44 +0200

    remove commented-out actions from headerData in navigation.ts

--- src/navigation.ts
+++ src/navigation.ts
@@ -19,6 +19,8 @@ export const headerData = {
       href: getPermalink('/contact'),
     },
   ],
+//    actions: [{ text: 'Download', href: 'https://github.com/arthelokyo/astrowind', target: '_blank' }],
+
 };
 
 export const footerData = {
