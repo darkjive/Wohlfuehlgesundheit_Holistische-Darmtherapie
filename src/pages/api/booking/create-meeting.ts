a0f432aac9dc37f84ec4eaa84626ca7c0791b679
Al Ain <cosmwave@gmail.com>
2025-11-02 11:17:09 +0100

    add booking pages

---
+++ src/pages/api/booking/create-meeting.ts
@@ -0,0 +1,7 @@
+settings: {
+  host_video: true,              // Host-Kamera an
+  participant_video: true,       // Teilnehmer-Kamera an
+  waiting_room: true,            // Warteraum aktiv
+  mute_upon_entry: true,         // Stummschalten beim Beitritt
+  join_before_host: false,       // Vor Host beitreten?
+}
